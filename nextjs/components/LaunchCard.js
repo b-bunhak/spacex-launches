3c2de176a1bce234454fac99cbae69b77c044938
Bruno Bunhak <b.bunhak@gmail.com>
2022-06-23 16:53:43 -0300

    Badge image and success status

--- nextjs/components/LaunchCard.js
+++ nextjs/components/LaunchCard.js
@@ -1,11 +1,14 @@
 import Link from 'next/link';
 
 import {
+	Box,
 	Card,
 	CardContent,
 	CardActions,
 	Typography,
 	Button,
+	Avatar,
+	alpha,
 } from '@mui/material';
 
 export default function LaunchCard({
@@ -13,17 +16,49 @@ export default function LaunchCard({
 	date,
 	details,
 	linkLabel,
+	success,
+	upcoming,
 	linkHref,
+	badgeSrc,
 	...props
 }) {
 	return (
-		<Card {...props}>
+		<Card
+			{...props}
+			sx={{
+				backgroundColor: (theme) => alpha(theme.palette.background.paper, 0.65),
+			}}
+		>
 			<CardContent>
-				<Typography variant="h4">{name}</Typography>
+				<Box display="flex">
+					{badgeSrc && (
+						<Avatar alt={`${name} patch`} src={badgeSrc} sx={{ mr: 1 }} />
+					)}
+
+					<Typography variant="h4">{name}</Typography>
+				</Box>
 
 				<Typography variant="h6">{new Date(date).toLocaleString()}</Typography>
 
 				{details && <Typography variant="h5">{details}</Typography>}
+
+				<Typography variant="h6" sx={{ display: 'flex', alignItems: 'center' }}>
+					<Box
+						sx={(theme) => ({
+							display: 'inline-block',
+							height: '0.75em',
+							width: '0.75em',
+							borderRadius: '50%',
+							bgcolor: upcoming
+								? theme.palette.grey[500]
+								: success
+								? theme.palette.success.main
+								: theme.palette.error.main,
+							mr: 1,
+						})}
+					/>
+					{upcoming ? 'Upcoming' : success ? 'Successful' : 'Failed'}
+				</Typography>
 			</CardContent>
 
 			{linkHref && linkLabel && (
