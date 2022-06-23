51d63aca982b520eb62384c9e340614cd71bcd4f
Bruno Bunhak <b.bunhak@gmail.com>
2022-06-23 00:23:37 -0300

    Google analytics 4

--- nextjs/pages/_app.js
+++ nextjs/pages/_app.js
@@ -1,6 +1,24 @@
+import { useEffect } from 'react';
+
 import Head from 'next/head';
+import { useRouter } from 'next/router';
+
+import * as gtag from '../lib/gtag';
 
 function MyApp({ Component, pageProps }) {
+	const router = useRouter();
+	useEffect(() => {
+		const handleRouteChange = (url) => {
+			gtag.pageView(url);
+		};
+		router.events.on('routeChangeComplete', handleRouteChange);
+		router.events.on('hashChangeComplete', handleRouteChange);
+		return () => {
+			router.events.off('routeChangeComplete', handleRouteChange);
+			router.events.off('hashChangeComplete', handleRouteChange);
+		};
+	}, [router.events]);
+
 	return (
 		<>
 			<Head>
---
+++ nextjs/pages/_document.js
@@ -0,0 +1,29 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+import { GA_TRACKING_ID } from '../lib/gtag';
+
+export default function Document() {
+	return (
+		<Html>
+			<Head />
+			<body>
+				<Main />
+				<NextScript />
+				<script
+					async
+					src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+				/>
+				<script
+					dangerouslySetInnerHTML={{
+						__html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}');
+          `,
+					}}
+				/>
+			</body>
+		</Html>
+	);
+}
