6d31a7a34fb97177ae6ea19cf9aee1cd33463970
Bruno Bunhak <b.bunhak@gmail.com>
2022-06-23 23:56:48 -0300

    Hotjar script

--- nextjs/pages/_document.js
+++ nextjs/pages/_document.js
@@ -2,6 +2,8 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID;
 const OPTIMIZE_CONTAINER_ID = process.env.NEXT_PUBLIC_OPTIMIZE_ID;
+const HOTJAR_HJID = process.env.NEXT_PUBLIC_HOTJAR_HJID;
+const HOTJAR_HJSV = process.env.NEXT_PUBLIC_HOTJAR_HJSV;
 
 export default function Document() {
 	return (
@@ -24,6 +26,22 @@ export default function Document() {
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', '${GA_TRACKING_ID}');
+          `,
+					}}
+				/>
+
+				{/* Hotjar */}
+				<script
+					dangerouslySetInnerHTML={{
+						__html: `
+						(function(h,o,t,j,a,r){
+							h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+							h._hjSettings={hjid:${HOTJAR_HJID},hjsv:${HOTJAR_HJSV}};
+							a=o.getElementsByTagName('head')[0];
+							r=o.createElement('script');r.async=1;
+							r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+							a.appendChild(r);
+						})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
           `,
 					}}
 				/>
